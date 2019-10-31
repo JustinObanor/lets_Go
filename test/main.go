b5f29ae342397c351503a3d13496428115297dad
justin obanor <justinobanor@gmail.com>
2019-10-31 16:26:53 +0300

    makes some canges

--- test/main.go
+++ test/main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	var wg sync.WaitGroup
-	x := []int{1, 3, 5, 4, 3, 6, 8, 5, 3, 5}
-	c := make(chan int)
-	wg.Add(len(x))
-	for _, v := range x {
-		go func(v int) {
-			defer wg.Done()
-			c <- v
-		}(v)
-	}
-	for i := 0; i < len(x); i++ {
-		fmt.Println(<-c)
-	}
-	wg.Wait()
+	
 }
+
+func isPalindrome(s string)bool{}
