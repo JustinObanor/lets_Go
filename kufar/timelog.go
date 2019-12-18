ad67a8077046ca613624688ac0b79cbc9b422a57
Justin Obanor <justinobanor@icloud.com>
2019-12-18 15:49:23 +0300

    adds puller and worker

--- kufar/timelog.go
+++ kufar/timelog.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"sync"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -21,7 +21,9 @@ const (
 	dbname   = "timestamp"
 )
 
+var wg sync.WaitGroup
 var db *sql.DB
+var w http.ResponseWriter
 
 func init() {
 	var err error
@@ -39,14 +41,8 @@ type DataLog struct {
 	CurrentFileTime int `json:"currentFileTime"`
 }
 
-type all int
-type loader int
-
-var wg sync.WaitGroup
-
-
-func (lo loader)ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	url := "http://worldclockapi.com/api/json/utc/now"
+//Worker func loads data to db
+func Worker(url string, c chan DataLog, d chan bool) {
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -63,30 +59,63 @@ func (lo loader)ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	s = append(s, data)
 
-		ticker := time.NewTicker(10 * time.Second)
-		c := make(chan int)
-		d := make(chan bool)
-		
-
-		go func() {
-			for _, v := range s {
-			select {
-			case <-ticker.C:
-				c <- v.CurrentFileTime
-			case <-d:
-				return
-			}
-		}
-			close(c)
-		}()
-
-		for v := range c{
-		_, err = db.Exec("INSERT INTO timetable (time) VALUES ($1)", v)
-		if err != nil {
-			panic(err)
-		}
+	for _, v := range s {
+		c <- DataLog{v.ID, v.CurrentFileTime}
 	}
 
+	ticker := time.NewTicker(10 * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			res, err := http.Get(url)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer res.Body.Close()
+
+			data := DataLog{}
+
+			b, err := ioutil.ReadAll(res.Body)
+
+			s := make([]DataLog, 0)
+
+			json.Unmarshal(b, &data)
+
+			s = append(s, data)
+
+			for _, v := range s {
+				c <- DataLog{v.ID, v.CurrentFileTime}
+			}
+
+		case <-d:
+			fmt.Fprint(w, "Worker finished work")
+			wg.Done()
+			return
+		}
+	}
+}
+
+//Puller retrieves data from chan
+func Puller(c chan DataLog, d chan bool) {
+	for {
+		select {
+		case v := <-c:
+			_, err := db.Exec("INSERT INTO timetable (time) VALUES ($1)", v.CurrentFileTime)
+			if err != nil {
+				panic(err)
+			}
+		case <-d:
+			fmt.Fprint(w, "Puller finished pulling")
+			wg.Done()
+			return
+		}
+	}
+}
+
+type row int
+
+func (ro row) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("select * from timetable order by id desc limit 1")
 	if err != nil {
 		panic(err)
@@ -94,29 +123,33 @@ func (lo loader)ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	defer rows.Close()
 
-	stamps := make([]DataLog, 0)
+	s := make([]DataLog, 0)
 	for rows.Next() {
-		stamp := DataLog{}
-		err = rows.Scan(&stamp.ID, &stamp.CurrentFileTime)
+		ctime := DataLog{}
+		err = rows.Scan(&ctime.ID, &ctime.CurrentFileTime)
 		if err != nil {
 			panic(err)
 		}
-		stamps = append(stamps, stamp)
+		s = append(s, ctime)
 	}
 	if err = rows.Err(); err != nil {
 		panic(err)
 	}
 
-wg.Add(1)
-	
-go func(){
-	for _, v := range stamps {
-		fmt.Fprintf(w, "Current Time : [ID : %d\t  Time : %v]\n", v.ID, v.CurrentFileTime)
-	}
-	wg.Done()
-}()
-wg.Wait()
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go func() {
+		for _, v := range s {
+			fmt.Fprintf(w, "Current Time : [ID : %d\t  Time : %v]\n", v.ID, v.CurrentFileTime)
+		}
+		wg.Done()
+	}()
+	wg.Wait()
 }
+
+type all int
 
 func (s all) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT * FROM timetable")
@@ -145,13 +178,19 @@ func (s all) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var s all
-	var lo loader
+	var o row
+	url := "http://worldclockapi.com/api/json/utc/now"
 
+	c := make(chan DataLog)
+	d := make(chan bool)
+
+	wg.Add(2)
+	go Worker(url, c, d)
+	go Puller(c, d)
 
 	mux := http.NewServeMux()
-	mux.Handle("/", lo)
+	mux.Handle("/", o)
 	mux.Handle("/all", s)
-
-
 	log.Fatal(http.ListenAndServe(":8080", mux))
+	wg.Wait()
 }
