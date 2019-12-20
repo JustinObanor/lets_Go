a252572d0342d79b60ce849a80cb08ac267841ed
Justin Obanor <justinobanor@icloud.com>
2019-12-20 15:14:22 +0300

    saves changes

--- test/main.go
+++ test/main.go
@@ -3,29 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var length int
-
-	fmt.Println("Input length :")
-	fmt.Scan(&length)
-
-	var width int
-
-	fmt.Println("Input width :")
-	fmt.Scan(&width)
-
-	var choice int
-
-	fmt.Println("Input choice :")
-	fmt.Scan(&choice)
-
-	calc(choice, length, width)
+	err := hey(1, 2, "yo")
+	fmt.Println(err)
 }
 
-func calc(choice, l, w int) {
-	switch choice {
-	case 1:
-		fmt.Println(l * w)
-	case 2:
-		fmt.Println(2 * (l + w))
-	}
+func hey(v ...interface{}) error {
+
 }
