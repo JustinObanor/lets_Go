3e180b533aaae3234533faf779bd52bc4a629636
Justin Obanor <justinobanor@icloud.com>
2020-01-09 14:03:29 +0300

    qwerty

--- test/main.go
+++ test/main.go
@@ -13,33 +13,14 @@ type ToDo struct{
 	Done bool
 }
 
-func IsNotDone(todo ToDo)bool{
-	return !todo.Done
-}
-
 func main() {
 	//making a template, and passing a function into it
 	tpl, err := template.New("template.html").Funcs(template.FuncMap{"IsNotDone":IsNotDone}).ParseFiles("template.html")
 	if err != nil{
 		log.Fatal("Cant expand template", err)
 		return
-	}
-	todos := []ToDo{
-		{"Study Go", false},
-		{"Attend lectures per web dev", false},
-		{"...", false},
-		{"Profit", false},
-	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		if r.Method == http.MethodPost{
-			param := r.FormValue("id")
-			index, _ := strconv.ParseInt(param, 10,0)
-			todos[index].Done = true
+        default:
+            fmt.Println("no value received")
+        }
 		}
-		err := tpl.Execute(w,todos)
-		if err != nil{
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil)) 
 }
