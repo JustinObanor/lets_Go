83d0ea3d59807e7b28f285bd2aceb9212c9b0101
Justin Obanor <justinobanor@icloud.com>
2020-02-25 11:37:59 +0300

    adds alll

--- test/main.go
+++ test/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+var location *time.Location
 
 func main() {
-	s := []byte("emma")
-	fmt.Printf("%p\n", s)
-	for i := range s {
-		fmt.Printf("%p\n", &s[i])
-	}
+	now := time.Now().UTC()
+	fmt.Println(now)
+	//12:35:09.331984 +0000 UTC
+
+	location, _ := time.LoadLocation("Europe/Minsk")
+
+	now3 := time.Now().UTC().In(location).Format(time.RFC1123)
+	fmt.Println(now3)
 }
