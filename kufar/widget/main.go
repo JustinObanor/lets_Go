764baab0da9687e9368ce312eb78194121f3bd03
Justin Obanor <justinobanor@icloud.com>
2020-03-31 14:35:22 +0300

    adds multiple consumers

--- kufar/widget/main.go
+++ kufar/widget/main.go
@@ -14,6 +14,7 @@ type widget struct {
 }
 
 var n = flag.Int("n", 1, "how many widgets produced by producer")
+var con = flag.Int("c", 1, "how many widgets consumed by consumer")
 var d = flag.Int64("d", 1000, "a consumer taking a while to process a widget")
 
 func main() {
@@ -21,29 +22,36 @@ func main() {
 	var wg sync.WaitGroup
 	flag.Parse()
 
-	for i := 1; i <= *n; i++ {
+	for i := 0 ; i <= *n; i++ {
 		wg.Add(1)
 		go func(num int) {
 			defer wg.Done()
 			c <- widget{label: "widget_" + strconv.Itoa(num), time: time.Now()}
 
 			tick := time.NewTicker(time.Millisecond * time.Duration(*d))
-			for range tick.C{
+			for range tick.C {
 				c <- widget{label: "widget_" + strconv.Itoa(num), time: time.Now()}
 			}
 		}(i)
 	}
 
-	go func(){
+	go func() {
 		wg.Wait()
 		close(c)
 	}()
 
-	consumer(c)
+	for i := 0; i <= *con; i++ {
+		wg.Add(1)
+		go func(num int){
+			defer wg.Done()
+			consumer(c, num)
+		}(i)
+	}
+	wg.Wait()
 }
 
-func consumer(c chan widget) {
+func consumer(c chan widget, con int) {
 	for elem := range c {
-		fmt.Printf("[%s  %v]\n", elem.label, elem.time.Format("15: 04: 05.0000"))
+		fmt.Printf("[%s  %v] consumer_%d\n", elem.label, elem.time.Format("15: 04: 05.0000"), con)
 	}
 }
