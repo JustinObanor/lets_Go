90991d6f9f979e8f63e6786b938d8bbcb4a9caea
JustinObanor <justinobanor@icloud.com>
2020-06-06 16:31:07 +0300

    adds all

--- practice/dataRace/fixed.go
+++ practice/dataRace/fixed.go
@@ -6,9 +6,10 @@ import (
 	"time"
 )
 
-func main() {
+func fixed() {
 	var counter int
 	var wg sync.WaitGroup
+	var mu sync.Mutex
 	const gr = 100
 
 	wg.Add(gr * 2)
@@ -16,13 +17,21 @@ func main() {
 	for i := 0; i < gr; i++ {
 		go func() {
 			time.Sleep(time.Millisecond * 100)
+
+			mu.Lock()
 			counter++
+			mu.Unlock()
+
 			wg.Done()
 		}()
 
 		go func() {
 			time.Sleep(time.Millisecond * 100)
+
+			mu.Lock()
 			counter--
+			mu.Unlock()
+
 			wg.Done()
 		}()
 	}
