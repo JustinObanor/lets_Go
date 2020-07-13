04dc46b49c9cae8544c9a9c01540b11139e67085
JustinObanor <justinobanor@icloud.com>
2020-07-13 12:15:29 +0300

    enables concurrent url aggregation

--- multiplexer/main.go
+++ multiplexer/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
@@ -13,8 +18,27 @@ type URLs struct {
 	List []string `json:"list"`
 }
 
+func getContent(client *http.Client, url string) ([]byte, error) {
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
 func main() {
 	var urls URLs
+	var wg sync.WaitGroup
+	var mu sync.RWMutex
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	client := http.Client{
 		Timeout: time.Second,
@@ -25,21 +49,56 @@ func main() {
 			return
 		}
 
-		for _, payLoad := range urls.List {
-			resp, err := client.Get(payLoad)
-			if err != nil {
-				w.Write([]byte(err.Error()))
-			}
-			defer resp.Body.Close()
-
-			b, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				w.Write([]byte(err.Error()))
-			}
-
-			w.Write(b)
+		if len(urls.List) > 20 {
+			w.Write([]byte("amount of url's exceeded limit"))
+			return
 		}
+
+		for _, url := range urls.List {
+			wg.Add(1)
+			go func(u string) {
+				mu.RLock()
+				content, err := getContent(&client, u)
+				mu.RUnlock()
+				
+				if err != nil {
+					w.Write([]byte(err.Error()))
+				}
+
+				mu.Lock()
+				w.Write(content)
+				mu.Unlock()
+
+				wg.Done()
+			}(url)
+		}
+		wg.Wait()
 	})
 
-	log.Println(http.ListenAndServe(":8080", nil))
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: nil,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen:%+s\n", err)
+		}
+	}()
+
+	log.Printf("server started")
+
+	<-done
+
+	log.Printf("server stopped")
+
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctxShutDown); err != nil {
+		log.Fatalf("server Shutdown Failed:%+s", err)
+	}
+
+	log.Printf("server exited properly")
+
 }
