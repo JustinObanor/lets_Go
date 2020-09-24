93f88094900a1825e4b028d3b187a43e287b6544
JustinObanor <justinobanor@icloud.com>
2020-09-24 16:59:57 +0300

    adds all

--- practice/sortingAlgorithms/mergeSort/mergeSort.go
+++ practice/sortingAlgorithms/mergeSort/mergeSort.go
@@ -12,24 +12,14 @@ func mergeSort(s []int) []int {
 		return s
 	}
 
-	fmt.Println("splitting", s)
 	mid := len(s) / 2
 
-	left := s[:mid]
-	right := s[mid:]
-	fmt.Println("left", left, "right", right)
-
-	return merge(mergeSort(left), mergeSort(right))
+	return merge(mergeSort(s[:mid]), mergeSort(s[mid:]))
 }
 
 func merge(left, right []int) []int {
 	size, leftIdx, rightIdx := len(left)+len(right), 0, 0
 	slice := make([]int, size, size)
-
-	fmt.Println("merging", left, "and", right)
-
-	fmt.Println("size", size, "lftIdx", leftIdx, "rgtIdx", rightIdx)
-	fmt.Println("left", left, "right", right)
 
 	for k := 0; k < size; k++ {
 		if leftIdx > len(left)-1 && rightIdx <= len(right)-1 {
