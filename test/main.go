0d639f91489507edfdffb44a113d6d6dd9ae505d
JustinObanor <justinobanor@icloud.com>
2020-10-19 12:23:53 +0300

    adds all

--- test/main.go
+++ test/main.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"io/ioutil"
 )
 
 func main() {
-	// open the main.go source file - likely this one
-	file, err := os.Open("testdata")
-	if err != nil {
-		log.Fatal(err)
+	f, err := ioutil.ReadDir("project")
+	if err != nil{
+		fmt.Println(err)
 	}
-
-	fInfo, err := file.Readdir(-1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, v := range fInfo {
-		fmt.Println(v.Name())
-	}
+	
+	fmt.Println(f)
 }
